de13696db953f0b5e9f7c85721c55ce33a6cb8f9
chiru7 <nagato3c+github@gmail.com>
2017-02-25 00:51:35 +0900

    add logger

---
+++ modules/logger.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var log4js = require('log4js');
+
+log4js.configure('./config/log4js.json');
+
+module.exports = {
+  access: log4js.getLogger('access'),
+  sequelize: log4js.getLogger('sequelize'),
+  system: log4js.getLogger('system'),
+  app: log4js.getLogger('app'),
+  express: log4js.connectLogger(log4js.getLogger('access'), {level: log4js.levels.INFO})
+};
